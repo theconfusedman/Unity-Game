82e0070a6de9610ce0ae6f8c4f4072048e3edf6d
theconfusedman <gopimelam82@gmail.com>
2023-02-05 21:50:27 -0500

    Camera tests

--- Maze Game/Assets/Scripts/GameManager.cs
+++ Maze Game/Assets/Scripts/GameManager.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{   
+{
+    
+    
     // Start is called before the first frame update
     void Start()
     {
