b08435379a01591ae1947c304c63955dcc5d4876
MeepTortoise <meeptortoise@gmail.com>
2023-02-10 00:49:29 -0500

    created enemy prefab

---
+++ Maze Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float moveDistance;
+    public float speed = 3;
+    private float startX;
+    private bool backwards = false;
+    private Rigidbody2D body;
+
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startX = body.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentX = body.position.x;
+        float currentY = body.position.y;
+
+        if (currentX - startX < moveDistance && !backwards)
+        {
+            float newPosition = currentX + (speed * Time.deltaTime);
+
+            print("moving forward to " + newPosition);
+            body.MovePosition(new Vector2(newPosition, currentY));
+
+        } else if (!backwards)
+        {
+
+            print("going backwards now");
+            backwards = true;
+
+        } else if (currentX - startX > 0)
+        {
+
+            float newPosition = currentX - (speed * Time.deltaTime);
+            print("moving backward");
+            body.MovePosition(new Vector2(newPosition, currentY));
+
+        } else
+        {
+
+            print("going forward");
+            backwards = false;
+
+        }
+    }
+}
